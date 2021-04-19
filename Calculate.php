c14739f9abf7beb5ab47f26f9ae9078928fba6c2
Tame394824 <tame765p@gmail.com>
2021-04-19 14:23:24 +0900

    プログラムを書く

---
+++ Calculate.php
@@ -0,0 +1,70 @@
+<?php
+
+$Formula = $_GET["Formula"];
+$InputCheck = "/^[0-9\+\-\*\/\(\)]+$/";
+if(preg_match($InputCheck, $Formula)){
+	$Number=str_split($Formula);
+	$Temp = array();
+	$TempCount = 0;
+	$Operation = array("+", "-", "*", "/");
+	foreach($Number as $Item){
+		if($Item == "("){
+			$TempCount++;
+		}else if(($Item == ")")){
+			$TempCount--;
+			$Temp[$TempCount] .= Calculation($Temp[$TempCount+1], $Operation);
+			$Temp[$TempCount+1]="";
+		}else{
+			$Temp[$TempCount] .= $Item;
+		}
+	}
+	$Answer = Calculation($Temp[0], $Operation);
+	echo $Answer;
+}else{
+	echo "ERROR";
+}
+
+function Calculation($Value, $Operation){
+	if(count($Operation)<=0){
+		return $Value;
+	}
+	$Op = array_shift($Operation);
+	$Number=str_split($Value);
+	$Temp = array();
+	$TempCount = 0;
+	foreach($Number as $Item){
+		if($Item == $Op){
+			$Temp[$TempCount] = Calculation($Temp[$TempCount], $Operation);
+			$TempCount ++;
+		}else{
+			$Temp[$TempCount] .= $Item;
+		}
+	}
+	$Temp[$TempCount] = Calculation($Temp[$TempCount], $Operation);
+	$Cal = array_shift($Temp);
+	switch ($Op) {
+		case "+":
+			foreach($Temp as $Item){
+				$Cal += floatval($Item);
+			}
+			break;
+		case "-":
+			foreach($Temp as $Item){
+				$Cal -= floatval($Item);
+			}
+			break;
+		case "*":
+			foreach($Temp as $Item){
+				$Cal *= floatval($Item);
+			}
+			break;
+		case "/":
+			foreach($Temp as $Item){
+				$Cal /= floatval($Item);
+			}
+			break;
+	}
+	return $Cal;
+}
+
+?>
